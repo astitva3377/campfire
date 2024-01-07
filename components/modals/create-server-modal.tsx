6f5c471eb12cbd5b35ec2fcac266bccf7f7ef475
Astitva Prakash <astitva0310@gmail.com>
2024-01-07 19:33:19 +0530

    Sidebar Components Ready

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -8,9 +8,9 @@ import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, D
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect, useState } from 'react';
 import { FileUpload } from '@/components/file-upload';
 import { useRouter } from 'next/navigation';
+import { useModal } from '@/hooks/use-modal-store';
 
 const formSchema = z.object({
 	name: z.string().min(1, {
@@ -21,13 +21,11 @@ const formSchema = z.object({
 	})
 });
 
-export const InitialModal = () => {
-	const [isMounted, setIsMounted] = useState(false);
+export const CreateServerModal = () => {
+	const { isOpen, onClose, type } = useModal();
 	const router = useRouter();
 
-	useEffect(() => {
-		setIsMounted(true);
-	}, []);
+	const isModalOpen = isOpen && type === "createServer";
 
 	const form = useForm({
 		resolver: zodResolver(formSchema),
@@ -43,16 +41,19 @@ export const InitialModal = () => {
 			await axios.post("/api/servers", values);
 			form.reset();
 			router.refresh();
-			window.location.reload();
+			onClose();
 		} catch (error) {
 			console.log(error);
 		}
 	}
 
-	if(!isMounted) return null;
+	const handleClose = () => {
+		form.reset();
+		onClose();
+	};
 
 	return (
-		<Dialog open>
+		<Dialog open={isModalOpen} onOpenChange={handleClose}>
 			<DialogContent className='bg-white text-black p-0 overflow-hidden'>
 				<DialogHeader className='pt-8 px-6'>
 					<DialogTitle className='text-center text-2xl font-bold'>Customize Your Server</DialogTitle>
--- components/modals/edit-server-modal.tsx
+++ components/modals/edit-server-modal.tsx
@@ -8,9 +8,10 @@ import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, D
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect, useState } from 'react';
 import { FileUpload } from '@/components/file-upload';
 import { useRouter } from 'next/navigation';
+import { useModal } from '@/hooks/use-modal-store';
+import { useEffect } from 'react';
 
 const formSchema = z.object({
 	name: z.string().min(1, {
@@ -21,13 +22,12 @@ const formSchema = z.object({
 	})
 });
 
-export const InitialModal = () => {
-	const [isMounted, setIsMounted] = useState(false);
+export const EditServerModal = () => {
+	const { isOpen, onClose, type, data } = useModal();
 	const router = useRouter();
 
-	useEffect(() => {
-		setIsMounted(true);
-	}, []);
+	const isModalOpen = isOpen && type === "editServer";
+	const { server } = data;
 
 	const form = useForm({
 		resolver: zodResolver(formSchema),
@@ -37,22 +37,32 @@ export const InitialModal = () => {
 		}
 	});
 
+	useEffect(() => {
+		if(server) {
+			form.setValue("name", server.name);
+			form.setValue("imageUrl", server.imageUrl);
+		}
+	}, [server, form]);
+
 	const isLoading = form.formState.isSubmitting;
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
 		try {
-			await axios.post("/api/servers", values);
+			await axios.patch(`/api/servers/${server?.id}`, values);
 			form.reset();
 			router.refresh();
-			window.location.reload();
+			onClose();
 		} catch (error) {
 			console.log(error);
 		}
 	}
 
-	if(!isMounted) return null;
+	const handleClose = () => {
+		form.reset();
+		onClose();
+	};
 
 	return (
-		<Dialog open>
+		<Dialog open={isModalOpen} onOpenChange={handleClose}>
 			<DialogContent className='bg-white text-black p-0 overflow-hidden'>
 				<DialogHeader className='pt-8 px-6'>
 					<DialogTitle className='text-center text-2xl font-bold'>Customize Your Server</DialogTitle>
@@ -82,7 +92,7 @@ export const InitialModal = () => {
 						</div>
 						<DialogFooter className='bg-gray-100 px-6 py-4'>
 							<Button variant="primary" disabled={isLoading}>
-								Create
+								Save
 							</Button>
 						</DialogFooter>
 					</form>
